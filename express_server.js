c9b9183dc9e791b9a3e9aebe9c415cd572f45938
Nick <pickme1152019@gmail.com>
2019-03-12 21:54:42 +0000

    added the bodyparser package and wrote the function to generate the random short URL

--- express_server.js
+++ express_server.js
@@ -1,8 +1,26 @@
-var express = require("express");
-var app = express();
-var PORT = 8080;
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const PORT = 8080;
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+function generateRandomString() {
+  let alphaNumeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  let output = ""
+
+  for(let i = 0; i < 6; i++) {
+    output += alphaNumeric.charAt(Math.floor(Math.random() * alphaNumeric.length));
+  }
+
+  return output;
+}
 
 
 var urlDatabase = {
@@ -10,12 +28,18 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
+  let templateVars = { urls: urlDatabase };
   res.render("urls_new", templateVars);
 });
 
@@ -24,9 +48,7 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
