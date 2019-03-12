673f540e16d3a6e4e99c9985473d22516610d5ee
Nick <pickme1152019@gmail.com>
2019-03-12 20:05:36 +0000

    added the functionality to list the urls in URL Database

--- express_server.js
+++ express_server.js
@@ -4,10 +4,16 @@ var PORT = 8080;
 
 app.set("view engine", "ejs");
 
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
